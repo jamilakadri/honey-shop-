c8b96bedf6d7c39bf6cb895fde76dd978078b8a4
jamilakadri <jamila.kadri3529@istic.ucar.tn>
2025-12-17 16:25:44 +0100

    Update production API URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://honey-shop-backend-production.up.railway.app/api'
-
-  imageBaseUrl: 'https://your-production-api.com'
+  apiUrl: 'https://honey-shop-backend-production.up.railway.app/api',
 };
