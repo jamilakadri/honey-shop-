ce7971f83e02263e0a933ed839b2ff235f832b6d
jamilakadri <jamila.kadri3529@istic.ucar.tn>
2025-12-19 08:42:59 +0100

    Emae

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiUrl: 'https://honey-shop-backend-production.up.railway.app/api' ,// ← URL de votre API .NET
-  imageBaseUrl: 'http://localhost:5198' 
+  imageBaseUrl: 'https://honey-shop-backend-production.up.railway.app/api' 
 };
